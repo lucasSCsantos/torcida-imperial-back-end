81e6da1fef762e52d966dac56340e027305ae3d3
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-24 12:46:23 -0300

    console.log

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,12 +4,18 @@ require('dotenv').config({
    path: path.resolve(__dirname, '../../.env') 
 });
 
+console.log({
+	user: 'admin',
+	password: process.env.PASSWORD,
+	host: process.env.HOST,
+	database: 'bd_told',
+})
+
 const connection = mysql.createPool({
 	user: 'admin',
 	password: process.env.PASSWORD,
 	host: process.env.HOST,
 	database: 'bd_told',
-	port: '3306'
 });
 
 module.exports = connection;
