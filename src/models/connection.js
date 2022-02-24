4ff054fe61918f06415a3f58e01b2fbb1cf5bd92
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-24 12:42:53 -0300

    VARS

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,14 +4,6 @@ require('dotenv').config({
    path: path.resolve(__dirname, '../../.env') 
 });
 
-console.log({
-	user: 'admin',
-	password: process.env.PASSWORD,
-	host: process.env.HOST,
-	database: 'bd_told',
-	port: '3306'
-})
-
 const connection = mysql.createPool({
 	user: 'admin',
 	password: process.env.PASSWORD,
